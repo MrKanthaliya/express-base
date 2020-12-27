60fe738b4ee60961b0c0111baffa5a03be3d0ffd
Pritesh Kanthaliya <prit.kanthaliya@gmail.com>
2020-12-28 00:09:43 +0530

    nyc code coverage cli fixed

--- src/validators/validation.js
+++ src/validators/validation.js
@@ -1,47 +1,25 @@
-const Joi = require('joi');
-import format from 'string-format';
-import xss from 'xss';
+const Joi = require("joi");
+import format from "string-format";
+import xss from "xss";
 
-
-const KEY_EMPTY = 'The key {} should not be empty.';
-const KEY_MISSING = 'The key {} should be present.';
-const KEY_NOT_VALID_OPTION = 'The key {} should be a valid option.';
-const INVALID_EMAIL = 'The key {} should be a valid email address.';
-const INVALID_MONGO_OBJECT_ID = 'The key {} should be a valid Mongo ObjectID.';
-const INVALID_TYPE = 'The key {} should be of type {}.';
-const INVALID_GUID = 'The key {} should be a valid GUID.';
-const STRING_OVER_MAXLENGTH = 'The key {} should be a maximum of {} characters long.';
-const NUMBER_NOT_NATURAL = 'The key {} should be a natural number.';
-const NUMBER_NOT_WHOLE = 'The key {} should be a whole number.';
-const NUMBER_OVER_LIMIT = 'The key {} should fall within an upper limit of {}.';
-const NUMBER_UNDER_LIMIT = 'The key {} should fall within a lower limit of {}.';
-// const DATE_NOT_ISO = 'The key {} should be a valid ISO 8601 formatted date.';
-// const DATE_NOT_YYYYMMDD = 'The key {} should be a valid date in YYYY:MM:DD format.';
-// const DISALLOWED_STRING = 'The key {} is a disallowed value';
-// const HEADER_MISSING = 'The header {} should be present.';
-// const HEADER_EMPTY = 'The header {} should not be empty.';
-// const HEADER_NOT_VALID_OPTION = 'The header {} should be a valid option.';
-// const INVALID_ALPHA_3_CODE = 'The key {} should be a valid ISO 3166 Alpha-3 code.';
-// const INVALID_BOOLEAN = 'The key {} should be a valid Boolean value.';
-// const INVALID_CURRENCY = 'The key {} should be a valid ISO 4217 currency code.';
-// const INVALID_HTTP_STATUS = 'The key {} should be a valid HTTP status code.';
-// const INVALID_JWT = 'The key {} should be a valid JWT.';
-// const INVALID_LOCALE = 'The key {} should be a valid ISO 639-2 locale.';
-// const INVALID_PHONE_CODE = 'The key {} should be a valid ISD phone code.';
-// const INVALID_PHONE_NUMBER = 'The key {} should be a valid phone number.';
-// const INVALID_PNG_IMAGE = 'The key {} should be a valid PNG image.';
-// const INVALID_PORT = 'The key {} should be a valid TCP port.';
-// const INVALID_TIMEZONE = 'The key {} should be a valid ISO 8601 timezone.';
-// const INVALID_URI = 'The key {} should be a valid URI.';
-// const NUMBER_NEGATIVE = 'The key {} should not be negative.';
-// const ARRAY_BELOW_MINLENGTH = 'The key {} should contain atleast {} entrie(s).';
-// const ARRAY_ABOVE_MAXLENGTH = 'The key {} can only have a max of {} entrie(s).';
+const KEY_EMPTY = "The key {} should not be empty.";
+const KEY_MISSING = "The key {} should be present.";
+const KEY_NOT_VALID_OPTION = "The key {} should be a valid option.";
+const INVALID_EMAIL = "The key {} should be a valid email address.";
+const INVALID_MONGO_OBJECT_ID = "The key {} should be a valid Mongo ObjectID.";
+const INVALID_TYPE = "The key {} should be of type {}.";
+const INVALID_GUID = "The key {} should be a valid GUID.";
+const STRING_OVER_MAXLENGTH = "The key {} should be a maximum of {} characters long.";
+const NUMBER_NOT_NATURAL = "The key {} should be a natural number.";
+const NUMBER_NOT_WHOLE = "The key {} should be a whole number.";
+const NUMBER_OVER_LIMIT = "The key {} should fall within an upper limit of {}.";
+const NUMBER_UNDER_LIMIT = "The key {} should fall within a lower limit of {}.";
 
 export const sanitizeXSS = (value) => {
     return xss(value, {
         whiteList: {}, // empty, means filter out all tags
         stripIgnoreTag: true, // filter out all HTML not in the whitelist
-        stripIgnoreTagBody: ['script'], // the script tag is a special case, we need
+        stripIgnoreTagBody: ["script"], // the script tag is a special case, we need
         // to filter out its content
     });
 };
@@ -51,13 +29,13 @@ export const string = (fieldName) =>
         .trim()
         .max(255)
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'string.base': format(INVALID_TYPE, fieldName, 'string'),
-            'string.empty': format(KEY_EMPTY, fieldName),
-            'string.max': format(STRING_OVER_MAXLENGTH, fieldName, '255'),
+            "any.required": format(KEY_MISSING, fieldName),
+            "string.base": format(INVALID_TYPE, fieldName, "string"),
+            "string.empty": format(KEY_EMPTY, fieldName),
+            "string.max": format(STRING_OVER_MAXLENGTH, fieldName, "255"),
         });
 
-export const stringSanitized = (fieldName) => string(fieldName).custom(sanitizeXSS, 'XSS sanitization');
+export const stringSanitized = (fieldName) => string(fieldName).custom(sanitizeXSS, "XSS sanitization");
 
 export const stringValue = (fieldName, values, defaultValue = null) => {
     const schema = Joi.string()
@@ -65,11 +43,11 @@ export const stringValue = (fieldName, values, defaultValue = null) => {
         .max(255)
         .valid(...values)
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'any.only': format(KEY_NOT_VALID_OPTION, fieldName),
-            'string.base': format(INVALID_TYPE, fieldName, 'string'),
-            'string.empty': format(KEY_EMPTY, fieldName),
-            'string.max': format(STRING_OVER_MAXLENGTH, fieldName, '255'),
+            "any.required": format(KEY_MISSING, fieldName),
+            "any.only": format(KEY_NOT_VALID_OPTION, fieldName),
+            "string.base": format(INVALID_TYPE, fieldName, "string"),
+            "string.empty": format(KEY_EMPTY, fieldName),
+            "string.max": format(STRING_OVER_MAXLENGTH, fieldName, "255"),
         });
 
     if (defaultValue) {
@@ -84,11 +62,11 @@ export const mongoObjectId = (fieldName) =>
         .hex()
         .length(24)
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'string.base': format(INVALID_TYPE, fieldName, 'string'),
-            'string.empty': format(KEY_EMPTY, fieldName),
-            'string.hex': format(INVALID_MONGO_OBJECT_ID, fieldName),
-            'string.length': format(INVALID_MONGO_OBJECT_ID, fieldName),
+            "any.required": format(KEY_MISSING, fieldName),
+            "string.base": format(INVALID_TYPE, fieldName, "string"),
+            "string.empty": format(KEY_EMPTY, fieldName),
+            "string.hex": format(INVALID_MONGO_OBJECT_ID, fieldName),
+            "string.length": format(INVALID_MONGO_OBJECT_ID, fieldName),
         });
 
 export const email = (fieldName) =>
@@ -97,11 +75,11 @@ export const email = (fieldName) =>
         .max(255)
         .email()
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'string.base': format(INVALID_TYPE, fieldName, 'string'),
-            'string.empty': format(KEY_EMPTY, fieldName),
-            'string.max': format(STRING_OVER_MAXLENGTH, fieldName, '255'),
-            'string.email': format(INVALID_EMAIL, fieldName),
+            "any.required": format(KEY_MISSING, fieldName),
+            "string.base": format(INVALID_TYPE, fieldName, "string"),
+            "string.empty": format(KEY_EMPTY, fieldName),
+            "string.max": format(STRING_OVER_MAXLENGTH, fieldName, "255"),
+            "string.email": format(INVALID_EMAIL, fieldName),
         });
 
 export const guid = (fieldName) =>
@@ -109,40 +87,40 @@ export const guid = (fieldName) =>
         .trim()
         .guid()
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'string.base': format(INVALID_TYPE, fieldName, 'string'),
-            'string.empty': format(KEY_EMPTY, fieldName),
-            'string.guid': format(INVALID_GUID, fieldName),
+            "any.required": format(KEY_MISSING, fieldName),
+            "string.base": format(INVALID_TYPE, fieldName, "string"),
+            "string.empty": format(KEY_EMPTY, fieldName),
+            "string.guid": format(INVALID_GUID, fieldName),
         });
 
 /**
-         * whole number validation.
-         * Validates if positive integer.
-         * @param {string} fieldName name of the key
-         */
-export const wholeNumber = function(fieldName) {
+ * whole number validation.
+ * Validates if positive integer.
+ * @param {string} fieldName name of the key
+ */
+export const wholeNumber = function (fieldName) {
     return Joi.number()
         .integer()
         .min(0)
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'number.integer': format(NUMBER_NOT_WHOLE, fieldName),
-            'number.min': format(NUMBER_NOT_WHOLE, fieldName),
+            "any.required": format(KEY_MISSING, fieldName),
+            "number.integer": format(NUMBER_NOT_WHOLE, fieldName),
+            "number.min": format(NUMBER_NOT_WHOLE, fieldName),
         });
 };
 /**
-         * Natural number validation.
-         * Validates if positive integer.
-         * @param {string} fieldName name of the key
-         */
-export const naturalNumber = function(fieldName) {
+ * Natural number validation.
+ * Validates if positive integer.
+ * @param {string} fieldName name of the key
+ */
+export const naturalNumber = function (fieldName) {
     return Joi.number()
         .integer()
         .positive()
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'number.integer': format(NUMBER_NOT_NATURAL, fieldName),
-            'number.min': format(NUMBER_NOT_NATURAL, fieldName),
+            "any.required": format(KEY_MISSING, fieldName),
+            "number.integer": format(NUMBER_NOT_NATURAL, fieldName),
+            "number.min": format(NUMBER_NOT_NATURAL, fieldName),
         });
 };
 
@@ -153,8 +131,8 @@ export const pageLimit = (fieldName) =>
         .max(100)
         .example(10)
         .messages({
-            'any.required': format(KEY_MISSING, fieldName),
-            'number.integer': format(NUMBER_NOT_NATURAL, fieldName),
-            'number.min': format(NUMBER_UNDER_LIMIT, fieldName, '10'),
-            'number.max': format(NUMBER_OVER_LIMIT, fieldName, '100'),
+            "any.required": format(KEY_MISSING, fieldName),
+            "number.integer": format(NUMBER_NOT_NATURAL, fieldName),
+            "number.min": format(NUMBER_UNDER_LIMIT, fieldName, "10"),
+            "number.max": format(NUMBER_OVER_LIMIT, fieldName, "100"),
         });
